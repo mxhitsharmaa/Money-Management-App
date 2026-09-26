87ec3d027edff50c9707bf1fb68fee94443aded3
Mohit Sharma <mxhitsharmaa@gmail.com>
2026-09-26 10:18:40 +0530

    Add config files to api branch

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "localhost";
+$dbname = "money_management";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+} catch (PDOException $e) {
+    http_response_code(500);
+    die("Database connection failed: " . $e->getMessage());
+}
